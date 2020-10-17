687c0b4edda18c6db61a4355c60c5710d91ccc6b
Garlic Fiend <suzannaakins@gmail.com>
2020-10-17 12:28:46 -0700

    res.json added to delete+post to append page

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 916;
 const app = express();
+const PORT = process.env.PORT || 9160;
 app.use(express.static('public'));
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
-const notes = require('./db/db.json');
+var notes = require('./db/db.json');
 
 // /notes returns the notes.html file
 app.get('/notes', (req, res) => {
@@ -21,8 +21,6 @@ app.get('/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-// The following API routes should be created:
 
 //GET function to make localhost:3001/api/notes reads db.json file + returns all saved notes as JSON
 app.get('/api/notes', (req, res) => {
@@ -35,18 +33,18 @@ app.post('/api/notes', (req, res) => {
     //set id of incoming data based on what next index of array will be
     newNote.id = notes.length.toString();
     notes.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('db/db.json', JSON.stringify(notes));
+    res.json(notes);
 });
 
 // Bonus
-// You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
-
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete('/api/notes/:id', (req, res) => {
-    var newArray = notes.filter(note => {
+    notes = notes.filter(note => {
         return note.id !== req.params.id
     });
-    fs.writeFileSync('./db/db.json', JSON.stringify(newArray));
+    fs.writeFileSync('db/db.json', JSON.stringify(notes));
+    res.json(notes);
 });
 
 app.listen(PORT, () => {
